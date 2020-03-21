a1bb4a6ecdce7d98fe6fcbe83e3141ef08d11393
TechsCode <paul@giese.in>
2020-03-21 02:49:24 +0100

    Experimental

--- src/main/java/me/TechsCode/GradleBasePlugin.java
+++ src/main/java/me/TechsCode/GradleBasePlugin.java
@@ -17,7 +17,6 @@ public class GradleBasePlugin implements Plugin<Project> {
         System.out.println("Applying Plugin "+(new File(".").getAbsolutePath()));
         System.out.println(project.getBuildDir().getAbsoluteFile().getAbsolutePath());
 
-        //project.getPluginManager().apply("com.github.johnrengelman.shadow");
         project.getDependencies().add("runtime", "com.github.jengelman.gradle.plugins:shadow:5.2.0");
         project.getPlugins().apply("com.github.johnrengelman.shadow");
         project.getTasksByName("build", false).stream().findFirst().get().dependsOn("shadowJar");
