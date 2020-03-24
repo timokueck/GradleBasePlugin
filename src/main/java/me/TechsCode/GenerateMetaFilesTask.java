707bc109e8440cd7cc6e1b8d57ea249be1237134
TechsCode <paul@giese.in>
2020-03-24 06:19:40 +0100

    Added BasePlugin dependency

--- src/main/java/me/TechsCode/GenerateMetaFilesTask.java
+++ src/main/java/me/TechsCode/GenerateMetaFilesTask.java
@@ -9,10 +9,10 @@ import java.io.*;
 public class GenerateMetaFilesTask extends DefaultTask {
 
     @TaskAction
-    public void generateMetaFiles(Project project){
+    public void generateMetaFiles(){
         System.out.println(Color.GREEN_BRIGHT+"Generating Plugin.yml & Bungee.yml");
 
-        File build = project.getBuildDir();
+        File build = getProject().getBuildDir();
         File pluginYml = new File(build.getAbsolutePath()+"/src/resources/plugin.yml");
         File bungeeYml = new File(build.getAbsolutePath()+"/src/resources/bungee.yml");
 
@@ -20,12 +20,12 @@ public class GenerateMetaFilesTask extends DefaultTask {
             pluginYml.createNewFile();
             bungeeYml.createNewFile();
 
-            MetaExtension meta = project.getExtensions().getByType(MetaExtension.class);
+            MetaExtension meta = getProject().getExtensions().getByType(MetaExtension.class);
 
             PrintWriter writer = new PrintWriter(pluginYml, "UTF-8");
-            writer.println("name: "+project.getName());
+            writer.println("name: "+getProject().getName());
             writer.println("version: "+meta.version);
-            writer.println("main: me.TechsCode."+project.getName()+".base.loader.SpigotLoader");
+            writer.println("main: me.TechsCode."+getProject().getName()+".base.loader.SpigotLoader");
             writer.println("api-version: 1.13");
             if(meta.loadAfter != null) writer.println("softdepend: "+meta.loadAfter);
             if(meta.loadBefore != null) writer.println("loadbefore: "+meta.loadBefore);
@@ -33,9 +33,9 @@ public class GenerateMetaFilesTask extends DefaultTask {
             writer.close();
 
             writer = new PrintWriter(bungeeYml, "UTF-8");
-            writer.println("name: "+project.getName());
+            writer.println("name: "+getProject().getName());
             writer.println("version: "+meta.version);
-            writer.println("main: me.TechsCode."+project.getName()+".base.loader.BungeeLoader");
+            writer.println("main: me.TechsCode."+getProject().getName()+".base.loader.BungeeLoader");
             writer.println("author: Tech");
             writer.close();
         } catch (IOException e) {
